6751c9e2994c47865320bde9bf300767705d2c10
CAIO SILVA MARQUES PORTO <caio.porto@csn.com.br>
2023-11-07 15:02:56 -0300

    alterado imagens de png para svg

--- src/app/components/experience/Experience.tsx
+++ src/app/components/experience/Experience.tsx
@@ -9,13 +9,13 @@ export default function Experience() {
             <SectionTitle text="Experience" />
             <p>1 year interning as Software Developer at Companhia Siderúrgica Nacional - CSN.</p>
             <div className="experience-time">
-                <Skill image="/bootstrap-logo.png" width={50} height={40} measure="3" years="3 years" />
-                <Skill image="/react-logo.png" width={50} height={43} measure="studying" years="Studying" study={true} />
-                <Skill image="/ts-logo.png" width={50} height={50} measure="studying" years="Studying" study={true} />
-                <Skill image="/java-logo.png" width={50} height={50} measure="1" years="1 year" />
-                <Skill image="/csharp-logo.png" width={50} height={50} measure="1" years="1 year" />
-                <Skill image="/net-logo.png" width={50} height={50} measure="1" years="1 year" />
-                <Skill image="/sql-logo.png" width={50} height={30} measure="2" years="2 years" />
+                <Skill image="/bootstrap-icon.svg" width={50} height={50} measure="3" years="3 years" />
+                <Skill image="/react-icon.svg" width={50} height={50} measure="studying" years="Studying" study={true} />
+                <Skill image="/ts-icon.svg" width={50} height={50} measure="studying" years="Studying" study={true} />
+                <Skill image="/java-icon.svg" width={50} height={50} measure="1" years="1 year" />
+                <Skill image="/csharp-icon.svg" width={50} height={50} measure="1" years="1 year" />
+                <Skill image="/net-icon.svg" width={50} height={50} measure="1" years="1 year" />
+                <Skill image="/sql-icon.svg" width={50} height={50} measure="2" years="2 years" />
             </div>
         </div>
     )
