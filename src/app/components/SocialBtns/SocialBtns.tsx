1fc6502f2f6c1fe91c84ad4744b7d60dc6ae134f
CAIO SILVA MARQUES PORTO <caio.porto@csn.com.br>
2023-11-07 13:44:37 -0300

    adicionado efeito hover nos botoes social

--- src/app/components/SocialBtns/SocialBtns.tsx
+++ src/app/components/SocialBtns/SocialBtns.tsx
@@ -4,16 +4,16 @@ import "./SocialBtns.scss"
 export default function SocialBtns() {
     return (
         <div className="social">
-            <a target="_blank" href="https://www.instagram.com/caiosilvamap/">
+            <a id="instagram" target="_blank" href="https://www.instagram.com/caiosilvamap/">
                 <Icon src="/insta-icon.svg" alt="icon insta" width={20} height={20} />
             </a>
-            <a target="_blank" href="https://www.linkedin.com/in/caiosilvamap/">
+            <a id="linkedin" target="_blank" href="https://www.linkedin.com/in/caiosilvamap/">
                 <Icon src="/linkedin-icon.svg" alt="icon LinkedIn" width={20} height={20} />
             </a>
-            <a target="_blank" href="https://github.com/caiosilvamap">
+            <a id="github" target="_blank" href="https://github.com/caiosilvamap">
                 <Icon src="/github-icon.svg" alt="icon GitHub" width={20} height={20} />
             </a>
-            <a target="_blank" href="https://wa.me/5524998331776">
+            <a id="whatsapp" target="_blank" href="https://wa.me/5524998331776">
                 <Icon src="/whatsapp-icon.svg" alt="icon WhatsApp" width={20} height={20} />
             </a>
         </div>
